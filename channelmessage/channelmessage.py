5c1ce7c7d22efc6cc428efba9c72c7f23afbd00a
ocformula <105538842+ocformula@users.noreply.github.com>
2023-05-31 02:05:18 +0900

    [ChannelMessage] update (#8)
    * Update __init__.py
    
    * Update channelmessage.py
    
    * Update info.json

--- channelmessage/channelmessage.py
+++ channelmessage/channelmessage.py
@@ -1,43 +1,19 @@
-"""
-MIT License
-
-Copyright (c) 2021-present Obi-Wan3
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import discord
 from redbot.core import commands
 
 
-class Announcements(commands.Cog):
-    """Announcement w/ Role Ping"""
+class ChannelMessage(commands.Cog):
+    """Send Messages as the bot!"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.admin_or_permissions(mention_everyone=True)
-    @commands.command(name="announcement")
-    async def _announcement(self, ctx: commands.Context, channel: discord.TextChannel, role: discord.Role, *, message=""):
-        """Send an announcement message to a specific channel with a role ping."""
+    @commands.is_owner()
+    @commands.command(name="channelmessage")
+    async def _channelmessage(self, ctx: commands.Context, server: discord.Guild, channel: discord.TextChannel, *, message=""):
+        """Send an message to a specific channel."""
         try:
-            await ctx.guild.get_channel(channel.id).send(f"{role.mention} {message}", allowed_mentions=discord.AllowedMentions(roles=True))
+            await channel.send(message)
             return await ctx.tick()
         except discord.Forbidden:
             return await ctx.send("I do not have permissions to send that message!")
