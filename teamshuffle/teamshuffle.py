d2203dc97c786c38bf59ff243a844ddf6b894c7b
ocformula <105538842+ocformula@users.noreply.github.com>
2023-05-31 18:53:49 +0900

    Add TeamShuffle (#9)
    * Create __init__.py
    
    * Create info.json
    
    * Create teamshuffle.py

---
+++ teamshuffle/teamshuffle.py
@@ -0,0 +1,30 @@
+import random
+import discord
+from redbot.core import commands
+
+
+class TeamShuffle(commands.Cog):
+    """Shuffles users into two teams."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="teamshuffle")
+    async def _teamshuffle(self, ctx, *args):
+        users = []
+        for arg in args:
+            users.append(arg)
+
+        if len(users) == 0:
+            await ctx.send("유저 목록을 입력해주세요.")
+            return
+            
+        if len(users) % 2 == 1:
+            users.append("")
+
+        random.shuffle(users)
+
+        teams = [users[i::2] for i in range(2)]
+
+        await ctx.send("1팀: {}".format(" ".join([str(user) for user in teams[0]])))
+        await ctx.send("2팀: {}".format(" ".join([str(user) for user in teams[1]])))
